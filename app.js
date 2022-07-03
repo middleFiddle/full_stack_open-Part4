8780cc1d10fa9fb0ea85ca58b47cb5540617874e
Ryan Gregory <ryan.middlefiddle@gmail.com>
2022-07-03 17:26:40 -0400

    exercises 4.1-4.2

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+    .then(result => console.log('connected to Mongo'))
+
+
+
+app.use(cors())
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+require('dotenv').config()
+const app = require('./app')
+
+
+
+const server = http.createServer(app)
+
+
+
+const PORT = process.env.PORT
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
